d30d632f4f31e266f8e42b10fd2e86a73073b410
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-10-03 18:06:06 -0300

    criando o helper de uploads de foto para os perfis e criação da pasta sessions

--- backend/utils/helpers/helps.js
+++
@@ -1,1 +0,0 @@
-console.log("crinado as pastas essenciais do projeto");
---
+++ backend/utils/helpers/upload.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import fs from 'fs';
+import multer from 'multer';
+
+const uploadDir = path.join(__dirname, '..', 'uploads');
+
+// Cria a pasta se não existir
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
