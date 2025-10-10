84df70d73778a3658120f7566ff8711a28d6e5a2
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-10-10 18:27:50 -0300

    Update resumeRoute.js

--- backend/routes/resumeRoute.js
+++ backend/routes/resumeRoute.js
@@ -4,15 +4,6 @@ import { createResume } from '../controllers/resumeController.js';
 
 const resumeRouter = express.Router();
 
-resumeRouter.get('/internship', (req, res) => {
-    res.render('template/internship', { layout: 'templates' });
-});
-
-resumeRouter.get('/work', (req, res) => {
-    res.render('template/work', { layout: 'templates' });
-});
-
-
 resumeRouter.post("/create-resume", checkAuth, createResume)
 
 export default resumeRouter;
